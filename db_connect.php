ef300a66242843b6684eaabc636a2b7bd6d9d978
jerwin1095 <168260731+jerwin1095@users.noreply.github.com>
2025-08-04 00:55:12 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,15 +1,12 @@
 <?php
-// Neon PostgreSQL connection
 $host     = 'ep-ancient-tree-af3pt6am-pooler.c-2.us-west-2.aws.neon.tech';
 $dbname   = 'neondb';
 $user     = 'neondb_owner';
 $password = 'npg_xD48zaycMqfl';
 $port     = '5432';
 
-// Create DB connection
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require");
 
-// Show error if connection fails
 if (!$conn) {
     die('Connection failed: ' . pg_last_error());
 }
